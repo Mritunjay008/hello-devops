d5d94ea753701d4e39d4e1d62af11a6d629a70e0
Mritunjay kumar <kumar.mritunjay2022@gmail.com>
2026-05-10 01:03:20 +0530

    fixing app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = process.loadEnvFile.PORT || 3000;
-app.get('/', (req, res)=>{
-    res.send('Hello world from Azure devops CI/CD');
-})
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on {PORT}`);
+app.get('/', (req, res) => {
+    res.send('Hello world from Azure DevOps CI/CD 🚀');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
